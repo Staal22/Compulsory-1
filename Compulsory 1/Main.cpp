622755a8a6c56affa94ed6620e8817cc6802ff59
Staal22 <staalbr@gmail.com>
2021-09-21 22:04:34 +0200

    cleanup and comment changes

--- Compulsory 1/Main.cpp
+++ Compulsory 1/Main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include<windows.h>                             //for Sleep() function, but only works on windows sadly
+#include<windows.h> //for Sleep() function (only works on windows)
 
 
 void clearCin();
@@ -24,9 +24,9 @@ int input{};
 
 std::string line = " ---   ---   ---\n";
 
-char table[row][col] = {                        //i would ideally like these to be empty or the same value (looks nicer)
-{' ', ' ', ' '},                                //but that makes it a pain to check for wincon so I'm not gonna bother
-{' ', ' ', ' '},                                //i could use the same method i already use and just ignore: ' '  but that would require a lot of extra unneccesary lines
+char table[row][col] = {                       
+{' ', ' ', ' '},                                
+{' ', ' ', ' '},                                
 {' ', ' ', ' '}
 };
 
@@ -34,13 +34,13 @@ char table[row][col] = {                        //i would ideally like these to 
 int main()
 {
     moves = 0;
-    while (gameover() == false) {               //keeps the game going as long as gameover() returns false, meaning no wincon or draw has been reached
+    while (gameover() == false) {        //keeps the game going as long as gameover() returns false, meaning no wincon or draw has been reached
         draw_board();
         turn();
         gameover();
     }
     if (player == 2 && draw == false) {         //when gameover returns true it goes to check who won or if it was a draw,
-        std::cout << "Player 1 wins! \n";       //1 and 2 are switched here, it's because i switch the player after every move and idk im too lazy to fix
+        std::cout << "Player 1 wins! \n";       //1 and 2 are switched here, it's because i switch the player after every move and idk I'm too lazy to fix
     }
     else if (player == 1 && draw == false) {
         std::cout << "Player 2 wins!! \n";      //technically speaking the game is a solved draw,
@@ -49,8 +49,8 @@ int main()
         std::cout << "It's a draw...\n";
 }
 
-char playersymbol(char symbol) {
-    if (symbol == 'X')
+char playersymbol(char symbol) {   //gets called once every turn to swap what symbol is being placed, but also inside the AI algorithm;,
+    if (symbol == 'X')             //it does this so it can do moves for both itself and opponent to look what is winning (actually it checks what is losing and tries to avoid that)
         symbol = 'O';
     else if (symbol == 'O')
         symbol = 'X';
@@ -58,10 +58,10 @@ char playersymbol(char symbol) {
     return symbol;
 }
 
-void draw_board()                               //prints the board
+void draw_board()    //prints the board
 {
-    system("cls");                              //potentially unsafe(?) but idk how else to clear the console
-                                                //not that clearing the console is a must, but it looks nice :)
+    system("cls");     //potentially unsafe(?) but idk how else to clear the console
+                       //not that clearing the console is a must, but it looks nice :)
     std::cout << line;
 
     for (int i = 0; i < row; i++) {
@@ -74,45 +74,39 @@ void draw_board()                               //prints the board
 
 }
 
-int getRandomNumber(double min, double max)
-{
-    static const double fraction = 1.0 / (static_cast <double> (RAND_MAX) + 1.0);
-    return static_cast<double>(rand() * fraction * (max - min + 1) + min);
-}
-
-
-bool gameover()                                 //gets called once after every turn to check if a wincon/draw has been reached, and if so returns gameover == true
+bool gameover()    //gets called once after every turn to check if a wincon/draw has been reached, and if so returns gameover == true
 {
 
     for (int i = 0; i < 3; i++)                 //checks for the six possible straight line wins
         if (table[i][0] == table[i][1] && table[i][0] == table[i][2] && table[i][0] != ' ' || table[0][i] == table[1][i] && table[0][i] == table[2][i] && table[0][i] != ' ')
             return true;
-                                                //cheks for the two possible diagonal wins
+                                                //checks for the two possible diagonal wins
     if (table[0][0] == table[1][1] && table[0][0] == table[2][2] && table[0][0] != ' ' || table[0][2] == table[1][1] && table[0][2] == table[2][0] && table[0][2] != ' ')
         return true;
 
-    if (moves < (row * col))                      //no wincon and less than nine moves means game isnt finished
+    if (moves < (row * col))    //no wincon and less than nine moves means game isn't finished
         return false;
 
-    draw = true;                                //(else) if all squares are taken and no wincon, the game is a draw
+    draw = true;             //(else) if all squares are taken and no wincon, the game is a draw
     return true;
 
 }
-
-int minimax(char table[row][col], int depth, bool isAI) {
+              //Minimax is a decision rule used in artificial intelligence for minimizing the possible loss for a worst case scenario (Wikipedia)
+              //this is c++ implementation for tic tac toe
+int minimax(char table[row][col], int depth, bool isAI) {  //the boolean value isAI is used to check if next move to be simulated is gonna be for the AI or the opponent
     int score = 0;
     int bestScore = 0;
     if (gameover() == true)
     {
         if (isAI == true)
-            return -1;
+            return -1; 
         if (isAI == false)
             return +1;
     }
     else
     {
-        if (depth < 9)
-        {
+        if (depth < 9)       //every time AI simulates a move depth increases by 1, this tells the algorithm how deep into the possibility tree it currently is,
+        {                    //but we don't need to go deeper than 9 levels to solve tic tac toe
             if (isAI == true)
             {
                 bestScore = -999;
@@ -120,11 +114,14 @@ int minimax(char table[row][col], int depth, bool isAI) {
                 {
                     for (int j = 0; j < col; j++)
                     {
-                        if (table[i][j] == ' ')
+                        if (table[i][j] == ' ')  //for all currently available moves (empty squares)
                         {
-                            table[i][j] = symbol;//simulates current move    
-                            score = minimax(table, depth + 1, false);
-                            table[i][j] = ' ';
+                            table[i][j] = symbol;//simulates current move for itself
+                                    
+//side note: if the order goes AI -> Player and then AI more than once in a row it seems to mess up the AI might have something to do with line above not calling playersymbol(), not sure
+
+                            score = minimax(table, depth + 1, false);//set isAI to false as next move will be for opponent
+                            table[i][j] = ' ';   //cleans up after itself (the move it imagines is never printed, just used to check for winning (or more accurately losing) lines)
                             if (score > bestScore)
                             {
                                 bestScore = score;
@@ -143,8 +140,8 @@ int minimax(char table[row][col], int depth, bool isAI) {
                     {
                         if (table[i][j] == ' ')
                         {
-                            table[i][j] = playersymbol(symbol);//imagines opponent answer, now as the opposite symbol
-                            score = minimax(table, depth + 1, true);
+                            table[i][j] = playersymbol(symbol);  //imagines opponent answer, now as the opposite symbol
+                            score = minimax(table, depth + 1, true);  //set isAI to true, next move will for AI
                             table[i][j] = ' ';
                             if (score < bestScore)
                             {
@@ -163,7 +160,7 @@ int minimax(char table[row][col], int depth, bool isAI) {
     }
 }
 
-int bestMove(char table[row][col], int moves)
+std::pair<int, int> bestMove(char table[row][col], int moves) //function that will give us those juicy x and y values for the perfect move
 {
     int x = -1, y = -1;
     int score = 0, bestScore = -999;
@@ -174,7 +171,7 @@ int bestMove(char table[row][col], int moves)
             if (table[i][j] == ' ')
             {
                 table[i][j] = symbol;
-                score = minimax(table, moves + 1, false);
+                score = minimax(table, moves + 1, false); //false here mean first move calculated will be for AI (see above)
                 table[i][j] = ' ';
                 if (score > bestScore)
                 {
@@ -185,31 +182,27 @@ int bestMove(char table[row][col], int moves)
             }
         }
     }
-    return x * 3 + y;
+    return std::make_pair(x, y); //return best possible move as a pair
 }
 
 
-void turn()                                    //gets input, puts symbol corresponding to player turn in corresponding field (assuming conditions are met), or asks player to go again
+void turn()   //gets input, puts symbol corresponding to player turn in corresponding field (assuming conditions are met), or asks player to go again
 {
-    std::cout << "Input 0 to have the AI do a move for you! or\n";
-                                               //just press 0 every other turn to play against AI
+    std::cout << "Input 0 to have the AI do a move for you! or\n";  //just press 0 every other turn to play against AI
+                
 
     std::cout << "Player " << player << " input a number from 1 to 9 to place your " << symbol << " : ";
 
     std::cin >> input;
 
-    int n{};
+    std::pair<int, int> bestmoves;
 
     switch (input) {
     case 0:
-    ai:
-        n = bestMove(table, moves);
-        x = n / row;
-        y = n % col;
-
-        //x = getRandomNumber(0, 2);             //yes, the "AI" is just playing a random move :P
-        //y = getRandomNumber(0, 2);
-        moves++;
+        bestmoves = bestMove(table, moves); //call bestMove to get a pair
+        x = bestmoves.first;  //set x = first pair value
+        y = bestmoves.second; //set y = second pair value
+        moves++;   //move counter to help AI know what move it is and also used to check for draw (move amount == board size => draw)
         break;
     case 1:
         x = 0;
@@ -263,33 +256,28 @@ void turn()                                    //gets input, puts symbol corresp
         turn();
     }
 
-                                                //change to X/O is field is empty, and swap player and symbol for next turn
+        //change to X/O is field is empty, and swap player and symbol for next turn
     if (player == 1 && table[x][y] == ' ') {
         table[x][y] = 'X';
         player = 2;
         symbol = playersymbol(symbol);
-        //symbol = 'O';
     }
     else if (player == 2 && table[x][y] == ' ') {
         table[x][y] = 'O';
         player = 1;
         symbol = playersymbol(symbol);
-        //symbol = 'X';
     }
     else {
-        //if (input == 0) {                       //prevents the AI from inputting into an already taken field
-        //    goto ai;
-        //}
         std::cout << "Field is already taken, try again\n";
         Sleep(1500);
         turn();
     }
 
-    draw_board();                               //call this again to show the move that was just done before next turn
+    draw_board();  //call this again to show the move that was just done before next turn
 
 }
 
-void clearCin()                                //this is a must to stop the program from looping if the user enters something other than an integer as input
+void clearCin()   //this is a must to stop the program from looping if the user enters something other than an integer as input
 {
     std::cin.clear();
     std::cin.ignore(32767, '\n');
